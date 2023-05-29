eefe000fc83ff350423186574ec85cae1fad7d18
csillaggyujto <48685792+csillaggyujto@users.noreply.github.com>
2023-05-29 16:34:39 +0000

    ISS81 - Wait for database online before attempting migration

--- OpenBudgeteer.Blazor/HostedDatabaseMigrator.cs
+++ OpenBudgeteer.Blazor/HostedDatabaseMigrator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using OpenBudgeteer.Data;
 using OpenBudgeteer.Data.Initialization;
+using OpenBudgeteer.Data.OnlineChecker;
 
 namespace OpenBudgeteer.Blazor;
 
@@ -13,21 +15,33 @@ public class HostedDatabaseMigrator : IHostedService
     private readonly DbContextOptions<DatabaseContext> _dbContextOptions;
     private readonly IConfiguration _configuration;
     private readonly IDatabaseInitializer _databaseInitializer;
+    private readonly IDatabaseOnlineChecker _onlineChecker;
 
     public HostedDatabaseMigrator(
         DbContextOptions<DatabaseContext> dbContextOptions,
         IConfiguration configuration,
-        IDatabaseInitializer databaseInitializer)
+        IDatabaseInitializer databaseInitializer,
+        IDatabaseOnlineChecker onlineChecker)
     {
         _dbContextOptions = dbContextOptions;
         _configuration = configuration;
         _databaseInitializer = databaseInitializer;
+        _onlineChecker = onlineChecker;
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         _databaseInitializer.InitializeDatabase(_configuration);
+        
+        // Wait for DB online
+        var isOnline = _onlineChecker.IsDbOnline(_configuration);
+        if (!isOnline)
+        {
+            throw new InvalidOperationException("Target database is not online.");
+        }
+        
         await using var context = new DatabaseContext(_dbContextOptions);
+
         await context.Database.MigrateAsync(cancellationToken: cancellationToken);
     }
 
