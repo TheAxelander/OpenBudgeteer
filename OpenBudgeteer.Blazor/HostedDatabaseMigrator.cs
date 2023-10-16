837a365495173e13f5c27ff8bddd1859f8ec1f76
Alexander Preibisch <alexpreib@outlook.com>
2023-10-16 16:10:06 +0200

    Add demo data load for new databases #192

--- OpenBudgeteer.Blazor/HostedDatabaseMigrator.cs
+++ OpenBudgeteer.Blazor/HostedDatabaseMigrator.cs
@@ -16,6 +16,8 @@ public class HostedDatabaseMigrator : IHostedService
     private readonly IConfiguration _configuration;
     private readonly IDatabaseInitializer _databaseInitializer;
     private readonly IDatabaseOnlineChecker _onlineChecker;
+    
+    private const string APPSETTINGS_DEMO_DATA = "APPSETTINGS_DEMO_DATA";
 
     public HostedDatabaseMigrator(
         DbContextOptions<DatabaseContext> dbContextOptions,
@@ -43,6 +45,9 @@ public class HostedDatabaseMigrator : IHostedService
         await using var context = new DatabaseContext(_dbContextOptions);
 
         await context.Database.MigrateAsync(cancellationToken: cancellationToken);
+
+        var initializeWithDemoData = _configuration.GetValue<bool>(APPSETTINGS_DEMO_DATA);
+        if (initializeWithDemoData) new DemoDataGenerator(_dbContextOptions).GenerateDemoData();
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
