3958d0dfb461f3e5a69599767b2964af3d088138
Alexander Preibisch <alexpreib@outlook.com>
2023-11-26 10:22:26 +0100

    Fix concurring Service blocks on parallel executions #205

--- OpenBudgeteer.Core.Data.Services/ServiceManager.cs
+++ OpenBudgeteer.Core.Data.Services/ServiceManager.cs
@@ -6,36 +6,20 @@ namespace OpenBudgeteer.Core.Data.Services;
 
 public class ServiceManager : IServiceManager
 {
-    public IAccountService AccountService => _lazyAccountService.Value;
-    public IBankTransactionService BankTransactionService => _lazyBankTransactionService.Value;
-    public IBucketGroupService BucketGroupService => _lazyBucketGroupService.Value;
-    public IBucketMovementService BucketMovementService => _lazyBucketMovementService.Value;
-    public IBucketService BucketService => _lazyBucketService.Value;
-    public IBucketRuleSetService BucketRuleSetService => _lazyBucketRuleSetService.Value;
-    public IBudgetedTransactionService BudgetedTransactionService => _lazyBudgetedTransactionService.Value;
-    public IImportProfileService ImportProfileService => _lazyImportProfileService.Value;
-    public IRecurringBankTransactionService RecurringBankTransactionService => _lazyRecurringBankTransactionService.Value;
+    public IAccountService AccountService => new AccountService(_dbContextOptions);
+    public IBankTransactionService BankTransactionService => new BankTransactionService(_dbContextOptions);
+    public IBucketGroupService BucketGroupService => new BucketGroupService(_dbContextOptions);
+    public IBucketMovementService BucketMovementService => new BucketMovementService(_dbContextOptions);
+    public IBucketService BucketService => new BucketService(_dbContextOptions);
+    public IBucketRuleSetService BucketRuleSetService => new BucketRuleSetService(_dbContextOptions);
+    public IBudgetedTransactionService BudgetedTransactionService => new BudgetedTransactionService(_dbContextOptions);
+    public IImportProfileService ImportProfileService => new ImportProfileService(_dbContextOptions);
+    public IRecurringBankTransactionService RecurringBankTransactionService => new RecurringBankTransactionService(_dbContextOptions);
     
-    private readonly Lazy<IAccountService> _lazyAccountService;
-    private readonly Lazy<IBankTransactionService> _lazyBankTransactionService;
-    private readonly Lazy<IBucketGroupService> _lazyBucketGroupService;
-    private readonly Lazy<IBucketMovementService> _lazyBucketMovementService;
-    private readonly Lazy<IBucketService> _lazyBucketService;
-    private readonly Lazy<IBucketRuleSetService> _lazyBucketRuleSetService;
-    private readonly Lazy<IBudgetedTransactionService> _lazyBudgetedTransactionService;
-    private readonly Lazy<IImportProfileService> _lazyImportProfileService;
-    private readonly Lazy<IRecurringBankTransactionService> _lazyRecurringBankTransactionService;
-
+    private readonly DbContextOptions<DatabaseContext> _dbContextOptions;
+    
     public ServiceManager(DbContextOptions<DatabaseContext> dbContextOptions)
     {
-        _lazyAccountService = new Lazy<IAccountService>(() => new AccountService(dbContextOptions));
-        _lazyBankTransactionService = new Lazy<IBankTransactionService>(() => new BankTransactionService(dbContextOptions));
-        _lazyBucketGroupService = new Lazy<IBucketGroupService>(() => new BucketGroupService(dbContextOptions));
-        _lazyBucketMovementService = new Lazy<IBucketMovementService>(() => new BucketMovementService(dbContextOptions));
-        _lazyBucketService = new Lazy<IBucketService>(() => new BucketService(dbContextOptions));
-        _lazyBucketRuleSetService = new Lazy<IBucketRuleSetService>(() => new BucketRuleSetService(dbContextOptions));
-        _lazyBudgetedTransactionService = new Lazy<IBudgetedTransactionService>(() => new BudgetedTransactionService(dbContextOptions));
-        _lazyImportProfileService = new Lazy<IImportProfileService>(() => new ImportProfileService(dbContextOptions));
-        _lazyRecurringBankTransactionService = new Lazy<IRecurringBankTransactionService>(() => new RecurringBankTransactionService(dbContextOptions));
+        _dbContextOptions = dbContextOptions;
     }
 }
