b27cf99343b6535338ced31f0c7790cb1b67dfff
Luca Berneking <luca@berneking.net>
2025-03-31 19:40:26 +0200

    Add Postgres Migration for BucketIsHiddenFromSummaries

---
+++ OpenBudgeteer.Core.Data.Postgres.Migrations/20250331173217_BucketIsHiddenFromSummaries.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenBudgeteer.Core.Data.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class BucketIsHiddenFromSummaries : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHiddenFromSummaries",
+                table: "Bucket",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+            
+            migrationBuilder.UpdateData(
+                table: "Bucket",
+                keyColumn: "BucketId",
+                keyValue: Guid.Parse("00000000-0000-0000-0000-000000000002"),
+                column: "IsHiddenFromSummaries",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsHiddenFromSummaries",
+                table: "Bucket");
+        }
+    }
+}
