cbbb7bdbc854dd7874d1b94162666444755c0b78
Alexander Preibisch <git@axelander.dev>
2025-12-22 13:08:25 +0100

    Fix Info page

--- OpenBudgeteer.Photino/Pages/Info.razor.cs
+++ OpenBudgeteer.Photino/Pages/Info.razor.cs
@@ -1,27 +1,25 @@
 using System.Collections.ObjectModel;
-using System.Data.Common;
+using DuckDB.NET.Data;
 using Microsoft.AspNetCore.Components;
-using OpenBudgeteer.Core.Data.Connection;
 
 namespace OpenBudgeteer.Photino.Pages;
 
 public partial class Info : ComponentBase
 {
     private record ThirdPartyInfo(string Name, string Url, string License, string LicenseUrl);
-    
-    [Inject] IDatabaseConnector<DbConnectionStringBuilder> DatabaseConnector { get; set; } = null!;
-    
+
+    [Inject] private DuckDBConnection DbConnection { get; set; } = null!;
+
+    private string DatabasePath => DbConnection.Database;
+
     private ObservableCollection<ThirdPartyInfo> Software => new()
     {
         new("aspnet-api-versioning", "https://github.com/dotnet/aspnet-api-versioning", "MIT", "https://github.com/dotnet/aspnet-api-versioning/blob/main/LICENSE.txt"),
         new("Blazor-ApexCharts", "https://github.com/apexcharts/Blazor-ApexCharts", "MIT", "https://github.com/apexcharts/Blazor-ApexCharts/blob/master/LICENSE"),
         new("dotnet-env", "https://github.com/tonerdo/dotnet-env", "MIT", "https://github.com/tonerdo/dotnet-env/blob/master/LICENSE"),
-        new("efcore", "https://github.com/dotnet/efcore", "MIT", "https://github.com/dotnet/efcore/blob/main/LICENSE.txt"),
-        new("efcore.pg", "https://github.com/npgsql/efcore.pg", "PostgreSQL License", "https://github.com/npgsql/efcore.pg/blob/main/LICENSE"),
+        new("DuckDB.NET", "https://github.com/Giorgi/DuckDB.NET", "MIT", "https://github.com/Giorgi/DuckDB.NET/blob/master/LICENSE"),
         new("MudBlazor", "https://github.com/MudBlazor/MudBlazor", "MIT", "https://github.com/MudBlazor/MudBlazor/blob/dev/LICENSE"),
-        new("Pomelo.EntityFrameworkCore.MySql", "https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql", "MIT", "https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql/blob/main/LICENSE"),
-        new("StackExchange.Redis", "https://github.com/StackExchange/StackExchange.Redis/", "MIT", "https://github.com/StackExchange/StackExchange.Redis/blob/main/LICENSE"),
-        new("Swashbuckle.AspNetCore", "https://github.com/domaindrivendev/Swashbuckle.AspNetCore", "MIT", "https://github.com/domaindrivendev/Swashbuckle.AspNetCore/blob/master/LICENSE"),
+        new("Photino.NET", "https://github.com/tryphotino/photino.NET", "Apache-2.0", "https://github.com/tryphotino/photino.NET/blob/main/LICENSE"),
         new("TinyCsvParser", "https://github.com/TinyCsvParser/TinyCsvParser", "MIT", "https://github.com/TinyCsvParser/TinyCsvParser/blob/master/LICENSE"),
         new("xunit", "https://github.com/xunit/xunit", "Apache-2.0", "https://github.com/xunit/xunit/blob/main/LICENSE"),
         new(".NET runtime", "https://github.com/dotnet/runtime", "MIT", "https://github.com/dotnet/runtime/blob/main/LICENSE.TXT"),
